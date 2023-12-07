913f9de19304a34aeaf363211ba3b3e108f4f93f
Conner Laursen <conner@Conners-MacBook-Air.local>
2023-12-06 20:02:20 -0800

    finished contact page minus thebutton and working on tab logo

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -46,7 +46,7 @@ function sendEmail(event) {
 
 function Contact() {
   return (
-
+    
     <div className="contactWrapper">
     <h2 className="contactHeader">Contact Us</h2>
     <h5 className="contactTitle"> <br></br> </h5>
@@ -56,43 +56,36 @@ function Contact() {
     necessary research will be done and then we will call you to discuss your 
     project, scope of work, and costs associated.  </p>
 
-
+<div className="contactDetails">
 <div className="contactInfo">
 <h4> Location: </h4>
 <p>1320 NW Mall Street, Suite B Issaquah, WA 98027 </p>
 <h4> Phone: </h4>
 <p>425.392.5351</p>
 <h4> Email: </h4>
-<p>info@eastsideconsultants.com</p>
-
-
+<a href="mailto:info@eastsideconsultants.com">info@eastsideconsultants.com</a>
 
 </div>
-<div className="map">
 
-
+<div>
+       
+        <img className="map"
+          src="https://maps.googleapis.com/maps/api/staticmap?center=1320+NW+Mall+Street,+Suite+B+Issaquah,+WA+98027&zoom=15&size=600x300&maptype=roadmap
+          &markers=color:red%7Clabel:C%7C1320+NW+Mall+Street,+Suite+B+Issaquah,+WA+98027
+          &key=AIzaSyCpWdbJyKutfMypMvjDwS5iHLNFHgL7S-M"
+          alt="Google Map"
+        />
+      </div>
+      </div>
+<div>
+<h4 className="bottomContactText">Rob Bogdon 
+described of our location as "we are strategically located 207 
+feet north of the Gas Lamp Tavern!" 
+</h4>
 </div>
 
  </div>
 
- 
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-     
    /* //   <div className="formcarry-container">
     //     <form onSubmit={sendEmail}>
     //       <input type="text" name="name" placeholder='Name' required />
