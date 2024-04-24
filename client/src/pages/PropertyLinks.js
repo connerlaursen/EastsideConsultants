4e17174ff0dc48cde5b297a4e2c0acecacc3b0fc
Conner Laursen <conner@Conners-MacBook-Air.local>
2024-04-24 00:07:54 -0700

    made property links open in a new tab

--- client/src/pages/PropertyLinks.js
+++ client/src/pages/PropertyLinks.js
@@ -10,21 +10,21 @@ function Projects() {
 <h2 className="propertyTitle">King County Department of Assessments: eReal Property</h2>
 
 <ul className='firstLink'>
-    <li><a href="https://kingcounty.gov/depts/assessor.aspx">King County Department of Assessments</a></li>
+    <li><a href="https://kingcounty.gov/depts/assessor.aspx" target="_blank" rel="noopener noreferrer">King County Department of Assessments</a></li>
     <li> Navigate to find parcel data, legal descriptions, land data, building data, and sales history.</li>
 </ul>
 
 <h2 className="propertyTitle">iMap (kingcounty.gov)</h2>
 
 <ul className='firstLink'>
-    <li><a href="https://kingcounty.gov/services/gis/maps/imap.aspx">iMap - King County GIS Center</a></li>
+    <li><a href="https://kingcounty.gov/services/gis/maps/imap.aspx" target="_blank" rel="noopener noreferrer">iMap - King County GIS Center</a></li>
     <li>Explore the interactive map to view aerial images, land contours, and environmentally sensitive areas.</li>
 </ul>
 
 <h2 className="propertyTitle">CFEDS.org</h2>
 
 <ul className='firstLink'>
-    <li><a href="https://cfsnsps.com/">CFEDS - Certified Federal Surveyor</a></li>
+    <li><a href="https://cfsnsps.com/" target="_blank" rel="noopener noreferrer">CFEDS - Certified Federal Surveyor</a></li>
     <li>Explore the site for details on becoming a Certified Federal Surveyor and related information.</li>
 </ul>
 <img className='img1' src={img4} alt="pic4" />
